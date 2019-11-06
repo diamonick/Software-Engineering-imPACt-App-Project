c3a6ffca7d0fa4dde019562991d0ca9a04c7fce5
Diamonick Dunn <ddunn12@lsu.edu>
2019-11-06 15:57:07 -0600

    Add Feed Page

---
+++ imPACt/imPACt/Pages/FeedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace imPACt.Pages
+{
+    public partial class FeedPage : ContentPage
+    {
+        public FeedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
