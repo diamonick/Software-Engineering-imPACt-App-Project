2216a47f83fca9fda0b755bc1de18ca677e10cbc
cmosol <46791335+cmosol@users.noreply.github.com>
2019-11-07 13:18:48 -0600

    add User Classification Page

---
+++ imPACt/imPACt/Pages/UserClassificationPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace imPACt.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserClassificationPage : ContentPage
+    {
+        private string Institution = "";        //String from institution entry
+        private string Location = "";           //String from location entry
+        private string[] CurrDegree = {};      //String array from current degree(s) entry
+        private string[] ProsDegree = {};       //String array from prospective degree(s) entry
+        private string[] ComplDegrees = {};     //String array from completed degrees(s) entry
+        private bool Bachelors = false;         //Boolean from degree entry
+        private bool Masters = false;           //Boolean from degree entry
+        private bool Phd = false;               //Boolean from degree entry
+        private bool Mentor = false;            //Boolean from Mentor selection
+        private bool Mentee = false;            //Boolean from Mentee selection
+
+        public UserClassificationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
