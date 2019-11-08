e0bea130f31403b90de243c5325c5f6c9778b7a2
Diamonick Dunn <ddunn12@lsu.edu>
2019-11-07 22:42:44 -0600

    Make custom icons and add functionality (WIP) to Feed Page

--- imPACt/imPACt/Pages/FeedPage.xaml.cs
+++ imPACt/imPACt/Pages/FeedPage.xaml.cs
@@ -7,9 +7,29 @@ namespace imPACt.Pages
 {
     public partial class FeedPage : ContentPage
     {
+
+        private const double PressedSize = 1.1;                     //Size of button when pressed
+        private int NumOfInterests = 0; 
+        private const int MaxNumOfInterests = 15;
+
         public FeedPage()
         {
             InitializeComponent();
+        }
+
+        async void HighlightButton(object sender, EventArgs args)
+        {
+            var imageButton = (ImageButton)sender;
+
+            await imageButton.ScaleTo(PressedSize, 200, Easing.SinOut);
+        }
+
+        async void SelectCategory(object sender, EventArgs args)
+        {
+            var imageButton = (ImageButton)sender;
+
+            
+            await imageButton.ScaleTo(1.0, 200, Easing.SinOut);
         }
     }
 }
--- imPACt/imPACt/Pages/UserClassificationPage.xaml.cs
+++ imPACt/imPACt/Pages/UserClassificationPage.xaml.cs
@@ -25,7 +25,7 @@ namespace imPACt.Pages
 
         public UserClassificationPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
         }
     }
 }
