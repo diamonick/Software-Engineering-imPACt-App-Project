704ba5a507863330e42eb8abddd982704177e054
cmosol <46791335+cmosol@users.noreply.github.com>
2019-11-19 11:47:03 -0600

    implement chat client "ChatPage.cs"

--- imPACt/imPACt/Models/Message.cs
+++ imPACt/imPACt/Models/Message.cs
@@ -6,10 +6,13 @@ namespace imPACt.Models
     public class Message
     {
         [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-        public int FromID { get; set; }
-        public int ToID { get; set; }
+        public String User { get; set; }
+        //public int ID { get; set; }
+        //public int FromID { get; set; }
+        //public int ToID { get; set; }
         public string MessageContent { get; set; }
-        public DateTime DateTimeSent { get; set; }
+        //public DateTime DateTimeSent { get; set; }
+        public bool IsSystemMessage;
+        public bool IsOwnMessage;
     }
 }
--- imPACt/imPACt/Pages/ChatPage.xaml.cs
+++ imPACt/imPACt/Pages/ChatPage.xaml.cs
@@ -13,6 +13,7 @@ namespace imPACt.Pages
     public partial class ChatPage : ContentPage
     {
         private string _name;
+        private int _id;
         private string _message;
         private ObservableCollection<Message> _messages;
         private bool _isConnected;
@@ -29,6 +30,19 @@ namespace imPACt.Pages
                 OnPropertyChanged();
             }
         }
+
+        //public int IdNum
+        //{
+        //    get
+        //    {
+        //        return _id;
+        //    }
+        //    set
+        //    {
+        //        _id = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
 
         public string Message
         {
@@ -74,11 +88,65 @@ namespace imPACt.Pages
         public Command ConnectCommand { get; }
         public Command DisconnectCommand { get; }
 
+        
+
 
         public ChatPage()
         {
             InitializeComponent();
 
+            Messages = new ObservableCollection<Message>();
+            SendMessageCommand = new Command(async () => { await SendMessage(Name, Message); });
+            ConnectCommand = new Command(async () => await Connect());
+            DisconnectCommand = new Command(async () => await Disconnect());
+
+            IsConnected = false;
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl($"http://localhost:44304")
+                .Build();
+
+            hubConnection.On<string>("JoinChat", (user) =>
+            {
+                Messages.Add(new Message() { User = Name, MessageContent = $"{user} has joined the chat", IsSystemMessage = true });
+            });
+
+            hubConnection.On<string>("LeaveChat", (user) =>
+            {
+                Messages.Add(new Message() { User = Name, MessageContent = $"{user} has left the chat", IsSystemMessage = true });
+            });
+
+            hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Messages.Add(new Message() { User = user, MessageContent = message, IsSystemMessage = false, IsOwnMessage = Name == user });
+            });
         }
+
+        async Task Connect()
+        {
+            await hubConnection.StartAsync();
+            await hubConnection.InvokeAsync("JoinChat", Name);
+
+            IsConnected = true;
+        }
+
+        async Task SendMessage(string user, string message)
+        {
+            await hubConnection.InvokeAsync("SendMessage", user, message);
+        }
+
+        async Task Disconnect()
+        {
+            await hubConnection.InvokeAsync("LeaveChat", Name);
+            await hubConnection.StopAsync();
+
+            IsConnected = false;
+        }
+
+        //protected override void OnPropertyChanged(
+        //[System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        //{
+            //OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
