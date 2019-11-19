3e252ec7dc711542144001f470601dc8defaf237
cmosol <46791335+cmosol@users.noreply.github.com>
2019-11-19 10:37:58 -0600

    fix SignalR packages/ add ChatPage

---
+++ imPACt/imPACt/Pages/ChatPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿
+using imPACt.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace imPACt.Pages
+{
+    public partial class ChatPage : ContentPage
+    {
+        private string _name;
+        private string _message;
+        private ObservableCollection<Message> _messages;
+        private bool _isConnected;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Message> Messages
+        {
+            get
+            {
+                return _messages;
+            }
+            set
+            {
+                _messages = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _isConnected;
+            }
+            set
+            {
+                _isConnected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private HubConnection hubConnection;
+        public Command SendMessageCommand { get; }
+        public Command ConnectCommand { get; }
+        public Command DisconnectCommand { get; }
+
+
+        public ChatPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
