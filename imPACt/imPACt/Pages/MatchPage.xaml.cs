ac5f5f21f1f02be50055b79b644bd7934d1473cb
cmosol <46791335+cmosol@users.noreply.github.com>
2019-12-04 01:57:27 -0600

    update formatting

--- imPACt/imPACt/Pages/MatchPage.xaml.cs
+++ imPACt/imPACt/Pages/MatchPage.xaml.cs
@@ -30,11 +30,49 @@ namespace imPACt.Pages
         {
             var Button = (Button)sender;
 
-            if (Button == FirstMatchButton) { FirstMatchButton.BackgroundColor = Color.FromHex("#B9E9F0"); }
-            else if (Button == SecondMatchButton) { SecondMatchButton.BackgroundColor = Color.FromHex("#B9E9F0"); }
-            else if (Button == ThirdMatchButton) { ThirdMatchButton.BackgroundColor = Color.FromHex("#B9E9F0"); }
-            else if (Button == FourthMatchButton) { FourthMatchButton.BackgroundColor = Color.FromHex("#B9E9F0"); }
-            else if (Button == FifthMatchButton) { FifthMatchButton.BackgroundColor = Color.FromHex("#B9E9F0"); }
+            if (Button == FirstMatchButton) { FirstMatchButton.BackgroundColor = Color.FromHex("#B9E9F0");
+                FirstDeclineButton.BackgroundColor = Color.FromHex("#ff708a");
+                FirstDeclineButton.IsEnabled = true;
+            }
+            else if (Button == SecondMatchButton) { SecondMatchButton.BackgroundColor = Color.FromHex("#B9E9F0");
+                SecondDeclineButton.BackgroundColor = Color.FromHex("#ff708a");
+                SecondDeclineButton.IsEnabled = true;
+            }
+            else if (Button == ThirdMatchButton) { ThirdMatchButton.BackgroundColor = Color.FromHex("#B9E9F0");
+                ThirdDeclineButton.BackgroundColor = Color.FromHex("#ff708a");
+                ThirdDeclineButton.IsEnabled = true;
+            }
+            else if (Button == FourthMatchButton) { FourthMatchButton.BackgroundColor = Color.FromHex("#B9E9F0");
+                FourthDeclineButton.BackgroundColor = Color.FromHex("#ff708a");
+                FourthDeclineButton.IsEnabled = true;
+            }
+            else if (Button == FifthMatchButton) { FifthMatchButton.BackgroundColor = Color.FromHex("#B9E9F0");
+                FifthDeclineButton.BackgroundColor = Color.FromHex("#ff708a");
+                FifthDeclineButton.IsEnabled = true;
+            }
+
+
+
+            if (Button == FirstDeclineButton) { FirstDeclineButton.BackgroundColor = Color.FromHex("#ffc2cd");
+                FirstMatchButton.BackgroundColor = Color.FromHex("#08B3C9");
+                FirstDeclineButton.IsEnabled = false;
+            }
+            else if (Button == SecondDeclineButton) { SecondDeclineButton.BackgroundColor = Color.FromHex("#ffc2cd");
+                SecondMatchButton.BackgroundColor = Color.FromHex("#08B3C9");
+                SecondDeclineButton.IsEnabled = false;
+            }
+            else if (Button == ThirdDeclineButton) { ThirdDeclineButton.BackgroundColor = Color.FromHex("#ffc2cd");
+                ThirdMatchButton.BackgroundColor = Color.FromHex("#08B3C9");
+                ThirdDeclineButton.IsEnabled = false;
+            }
+            else if (Button == FourthDeclineButton) { FourthDeclineButton.BackgroundColor = Color.FromHex("#ffc2cd");
+                FourthMatchButton.BackgroundColor = Color.FromHex("#08B3C9");
+                FourthDeclineButton.IsEnabled = false;
+            }
+            else if (Button == FifthDeclineButton) { FifthDeclineButton.BackgroundColor = Color.FromHex("#ffc2cd");
+                FifthMatchButton.BackgroundColor = Color.FromHex("#08B3C9");
+                FifthDeclineButton.IsEnabled = false;
+            }
         }
 
         void EnableMatchedTag(object sender, EventArgs args)
@@ -51,22 +89,54 @@ namespace imPACt.Pages
                 secondMatchTagBackground.IsVisible = true;
                 secondMatchTagLabel.IsVisible = true;
             }
-            else if (Button == SecondMatchButton)
+            else if (Button == ThirdMatchButton)
             {
                 thirdMatchTagBackground.IsVisible = true;
                 thirdMatchTagLabel.IsVisible = true;
             }
-            else if (Button == SecondMatchButton)
+            else if (Button == FourthMatchButton)
             {
                 fourthMatchTagBackground.IsVisible = true;
                 fourthMatchTagLabel.IsVisible = true;
             }
-            else if (Button == SecondMatchButton)
+            else if (Button == FifthMatchButton)
             {
                 fifthMatchTagBackground.IsVisible = true;
                 fifthMatchTagLabel.IsVisible = true;
             }
         }
+
+        void DisableMatchedTag(object sender, EventArgs args)
+        {
+            var Button = (Button)sender;
+
+            if (Button == FirstDeclineButton)
+            {
+                firstMatchTagBackground.IsVisible = false;
+                firstMatchTagLabel.IsVisible = false;
+            }
+            else if (Button == SecondDeclineButton)
+            {
+                secondMatchTagBackground.IsVisible = false;
+                secondMatchTagLabel.IsVisible = false;
+            }
+            else if (Button == ThirdDeclineButton)
+            {
+                thirdMatchTagBackground.IsVisible = false;
+                thirdMatchTagLabel.IsVisible = false;
+            }
+            else if (Button == FourthDeclineButton)
+            {
+                fourthMatchTagBackground.IsVisible = false;
+                fourthMatchTagLabel.IsVisible = false;
+            }
+            else if (Button == FifthDeclineButton)
+            {
+                fifthMatchTagBackground.IsVisible = false;
+                fifthMatchTagLabel.IsVisible = false;
+            }
+        }
+
 
         async void GoToMentorPage(object sender, EventArgs args)
         {
